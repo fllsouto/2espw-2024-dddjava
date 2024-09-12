edd61911bd99f00d8d310005156979d2a37268bf
Fellipe Souto <fllsouto@gmail.com>
2024-09-11 22:50:57 -0300

    wk2-cmt4: index out of bound problem

--- libuncleotidio/src/main/java/br/com/fiap/twoespw/libuncleotidio/App.java
+++ libuncleotidio/src/main/java/br/com/fiap/twoespw/libuncleotidio/App.java
@@ -8,6 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(":: Lib Uncle Otidio ::");
+        String entrada = "ACGTACGT";
+
+        String[] entradaSplitada = entrada.split("");
+        
+        for(int i = 0; i <= entradaSplitada.length; i++) {
+            System.out.println("[i] = [" + i + "]");
+            String caracter = entradaSplitada[i];
+            System.out.println("caracter: " + caracter);
+        }
     }
 }
