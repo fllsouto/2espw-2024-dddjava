07265cd3b8ac572e597af9bb63f846593d968af2
Fellipe Souto <fllsouto@gmail.com>
2024-09-11 23:03:43 -0300

    wk2-cmt5: adding A counting and etc

--- libuncleotidio/src/main/java/br/com/fiap/twoespw/libuncleotidio/App.java
+++ libuncleotidio/src/main/java/br/com/fiap/twoespw/libuncleotidio/App.java
@@ -9,14 +9,24 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(":: Lib Uncle Otidio ::");
-        String entrada = "ACGTACGT";
+        String entrada = "ACGTACGTAAAAAAAAAAAAAAAAAAAAA";
+
+        int frequenciaA = 0;
 
         String[] entradaSplitada = entrada.split("");
         
-        for(int i = 0; i <= entradaSplitada.length; i++) {
+        for(int i = 0; i <= entradaSplitada.length - 1; i++) {
             System.out.println("[i] = [" + i + "]");
             String caracter = entradaSplitada[i];
             System.out.println("caracter: " + caracter);
+
+            if (caracter.equals("A")) {
+                frequenciaA = frequenciaA + 1; 
+            }
         }
+
+        System.out.println("Sequência :: " + entrada);
+        System.out.println("Tamanho da Sequência :: " + entrada.length());
+        System.out.println("Total de A :: " + frequenciaA);
     }
 }
