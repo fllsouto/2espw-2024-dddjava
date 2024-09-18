f7ccf5502bb7ea6ba443c54f9513679cb3bc9ae2
Fellipe Souto <fllsouto@gmail.com>
2024-09-17 22:10:33 -0300

    wk3-cmt2: extracting for an external class

--- libuncleotidio/src/main/java/br/com/fiap/twoespw/libuncleotidio/App.java
+++ libuncleotidio/src/main/java/br/com/fiap/twoespw/libuncleotidio/App.java
@@ -8,43 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println(":: Lib Uncle Otidio ::");
         String entrada = "ACTGACTGACTGACTG";
 
-        int frequenciaA = 0;
-        int frequenciaC = 0;
-        int frequenciaT = 0;
-        int frequenciaG = 0;
+        NucleotidioCounter nc = new NucleotidioCounter();
 
-        String[] entradaSplitada = entrada.split("");
-        
-        for(int i = 0; i <= entradaSplitada.length - 1; i++) {
-            System.out.println("[i] = [" + i + "]");
-            String caracter = entradaSplitada[i];
-            System.out.println("caracter: " + caracter);
-
-            if (caracter.equals("A")) {
-                frequenciaA = frequenciaA + 1; 
-            }            
-
-            if (caracter.equals("C")) {
-                frequenciaC = frequenciaC + 1; 
-            }            
-
-            if (caracter.equals("T")) {
-                frequenciaT = frequenciaT + 1; 
-            }            
-            
-            if (caracter.equals("G")) {
-                frequenciaG = frequenciaG + 1; 
-            }
-        }
-
-        System.out.println("Sequência :: " + entrada);
-        System.out.println("Tamanho da Sequência :: " + entrada.length());
-        System.out.println("Total de A :: " + frequenciaA);
-        System.out.println("Total de C :: " + frequenciaC);
-        System.out.println("Total de T :: " + frequenciaT);
-        System.out.println("Total de G :: " + frequenciaG);
+        nc.count(entrada);
     }
 }
---
+++ libuncleotidio/src/main/java/br/com/fiap/twoespw/libuncleotidio/NucleotidioCounter.java
@@ -0,0 +1,44 @@
+package br.com.fiap.twoespw.libuncleotidio;
+
+public class NucleotidioCounter {
+    
+    public void count(String entrada) {
+        System.out.println(":: Lib Uncle Otidio ::");
+
+        int frequenciaA = 0;
+        int frequenciaC = 0;
+        int frequenciaT = 0;
+        int frequenciaG = 0;
+
+        String[] entradaSplitada = entrada.split("");
+        
+        for(int i = 0; i <= entradaSplitada.length - 1; i++) {
+            System.out.println("[i] = [" + i + "]");
+            String caracter = entradaSplitada[i];
+            System.out.println("caracter: " + caracter);
+
+            if (caracter.equals("A")) {
+                frequenciaA = frequenciaA + 1; 
+            }            
+
+            if (caracter.equals("C")) {
+                frequenciaC = frequenciaC + 1; 
+            }            
+
+            if (caracter.equals("T")) {
+                frequenciaT = frequenciaT + 1; 
+            }            
+            
+            if (caracter.equals("G")) {
+                frequenciaG = frequenciaG + 1; 
+            }
+        }
+
+        System.out.println("Sequência :: " + entrada);
+        System.out.println("Tamanho da Sequência :: " + entrada.length());
+        System.out.println("Total de A :: " + frequenciaA);
+        System.out.println("Total de C :: " + frequenciaC);
+        System.out.println("Total de T :: " + frequenciaT);
+        System.out.println("Total de G :: " + frequenciaG);
+    }
+}
